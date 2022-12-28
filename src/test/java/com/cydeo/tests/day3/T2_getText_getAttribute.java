3e741b6926db3b1d338c310941213268a8ac6fec
MuratYasul <m_yasul@hotmail.com>
2022-12-28 17:06:20 +0300

    webdriverfactory created

---
+++ src/test/java/com/cydeo/tests/day3/T2_getText_getAttribute.java
@@ -0,0 +1,33 @@
+package com.cydeo.tests.day3;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class T2_getText_getAttribute {
+    public static void main(String[] args) {
+        //TC #1: NextBaseCRM, Locators and getText() practice
+        //1. Open a Chrome browser
+        // WebDriverManager.chromedriver().setup();
+        // WebDriver driver = new ChromeDriver();
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+
+        //2. Go to: https://login1.nextbasecrm.com/
+        driver.get("https://login1.nextbasecrm.com/");
+
+        //3. Verify "remember me" label text is as expected:
+        //Expected : Remember me on this computer
+
+        WebElement rememberMeLabel = driver.findElement(By.className("login-item-checkbox-label"));
+
+        //4. Verify "forgot password" link text is as expected:
+        //Expected: Forgot your password?
+
+
+
+        //5.Verify "forgot password" href attribute's value contains expected:
+        //Expected: forgot_password=yes
+    }
+}
